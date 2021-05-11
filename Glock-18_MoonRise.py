a9120504d18c63357f73abc3e36b6cf2c8f421d0
MarcusKLuciano <MarcusKLuciano@users.noreply.github.com>
2021-05-11 18:54:56 +0530

    Create Glock-18_MoonRise.py

---
+++ Glock-18_MoonRise.py
@@ -0,0 +1,8 @@
+StarPattern = [19, 31, 38, 58, 59, 66, 90, 95, 102, 109, 121, 137, 142,
+               165, 194, 224, 237, 268, 280, 302, 355, 385, 403, 448,
+               475, 484, 487, 509, 601, 617, 628, 630, 667, 694, 706,
+               737, 744, 769, 800, 837, 864, 913, 958, 986]
+seed = 0
+while seed not in StarPattern:
+    seed = input("Enter The seed of your Glock Moonrise: ")
+print("Your input seed is a Star Pattern.")
